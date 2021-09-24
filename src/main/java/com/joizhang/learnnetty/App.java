01d14cd418a4cde06390da9d64dba44dcc48805d
joizhang <zxlwhom@outlook.com>
2021-09-24 21:15:03 +0800

    feat: Add AIO.

---
+++ src/main/java/com/joizhang/learnnetty/App.java
@@ -0,0 +1,10 @@
+package com.joizhang.learnnetty;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
