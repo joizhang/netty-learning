9be3f7d0bdcd1f5e851cd8620b67402ce24745f2
joizhang <zxlwhom@outlook.com>
2021-09-24 20:57:28 +0800

    feat: Add BIO time server.

---
+++ src/main/java/com/joizhang/learnnetty/bio/TimeServer.java
@@ -0,0 +1,47 @@
+package com.joizhang.learnnetty.bio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class TimeServer {
+
+    public static final ThreadPoolExecutor EXECUTOR = new ThreadPoolExecutor(
+            8, 8, 0, TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(200),
+            Executors.defaultThreadFactory(),
+            new ThreadPoolExecutor.AbortPolicy());
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server is start in port : " + port);
+            Socket socket = null;
+            while (true) {
+                socket = server.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        } finally {
+            if (server != null) {
+                System.out.println("The time server close");
+                server.close();
+            }
+        }
+    }
+
+}
